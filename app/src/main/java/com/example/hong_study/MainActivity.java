b53b30421641563c7d4991a1a565a911938a5d24
maker-jun <nicejun0524@gmail.com>
2019-12-31 18:29:47 +0900

    copy hongdroidnote
    copy

--- app/src/main/java/com/example/hong_study/MainActivity.java
+++ app/src/main/java/com/example/hong_study/MainActivity.java
@@ -3,12 +3,33 @@ package com.example.hong_study;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn_test;
+    EditText et_test;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        et_test=(EditText)findViewById(R.id.et_test);
+
+        btn_test = (Button)findViewById(R.id.btn_test);
+
+        btn_test.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                //btn_test.setText("hong");
+                et_test.setText("hong");
+            }
+                                    }
+
+        );
     }
 }
